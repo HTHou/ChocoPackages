844de92ad600f2e43df3a6d1e49fb7137e83e36d
HTHou <hhaonan@outlook.com>
2023-06-20 12:27:19 +0800

    add 1.82

---
+++ boost-msvc-14.3-1.82.0/tools/chocolateyuninstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$installDirRoot = "c:\local\boost_1_82_0"
+$binaryDirs = "$installDirRoot\lib32-msvc-14.3", "$installDirRoot\lib64-msvc-14.3"
+
+# remove binary directories
+foreach($binaryDir in $binaryDirs)
+{
+	write-host "Uninstalling boost will remove $binaryDir." -ForegroundColor Yellow
+	if (Test-Path "$binaryDir") { rm -Recurse -Force "$binaryDir" }
+}
+
+# remove root directory if there are no binary directories
+if (-not (Test-Path "$installDirRoot\lib32*") -and -not (Test-Path "$installDirRoot\lib64*")) { rm -Recurse -Force "$installDirRoot" }
