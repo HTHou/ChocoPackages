c5e518728ef9a2ceb905c77501cf38b4358beb7a
Haonan <hhaonan@outlook.com>
2024-03-08 11:18:39 +0800

    Update url 1.84.0

--- boost-msvc-14.3-1.84.0/tools/chocolateyInstall.ps1
+++ boost-msvc-14.3-1.84.0/tools/chocolateyInstall.ps1
@@ -2,8 +2,8 @@ Install-ChocolateyPackage `
     -packageName 'boost-msvc-14.3' `
     -installerType 'exe' `
     -silentArgs '/VERYSILENT' `
-    -url 'https://archives.boost.io/release/1.84.0/binaries/boost_1_84_0-msvc-14.3-32.exe' `
-    -url64bit 'https://archives.boost.io/release/1.84.0/binaries/boost_1_84_0-msvc-14.3-64.exe' `
+    -url 'https://boostorg.jfrog.io/artifactory/main/release/1.84.0/binaries/boost_1_84_0-msvc-14.3-32.exe' `
+    -url64bit 'https://boostorg.jfrog.io/artifactory/main/release/1.84.0/binaries/boost_1_84_0-msvc-14.3-64.exe' `
     -checksumType 'sha256' `
     -checksumType64 'sha256' `
     -checksum '5403937efb805906acbb34fc1693b898818f6388506ff0f4f0c02e5cb7aef554' `
