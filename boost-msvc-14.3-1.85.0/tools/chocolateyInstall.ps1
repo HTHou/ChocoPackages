9290adf9da3c8fc910bae15090423eede0a3e82d
HTHou <hhaonan@outlook.com>
2024-07-23 15:10:20 +0800

    upload 1.85.0

---
+++ boost-msvc-14.3-1.85.0/tools/chocolateyInstall.ps1
@@ -0,0 +1,12 @@
+Install-ChocolateyPackage `
+    -packageName 'boost-msvc-14.3' `
+    -installerType 'exe' `
+    -silentArgs '/VERYSILENT' `
+    -url 'https://boostorg.jfrog.io/artifactory/main/release/1.85.0/binaries/boost_1_85_0-msvc-14.3-32.exe' `
+    -url64bit 'https://boostorg.jfrog.io/artifactory/main/release/1.85.0/binaries/boost_1_85_0-msvc-14.3-64.exe' `
+    -checksumType 'sha256' `
+    -checksumType64 'sha256' `
+    -checksum 'e51285f8046dca5417ca0fa3a6b1c8cde7c621753dbdbde4731e0f8d5340fa5f' `
+    -checksum64 '7b43d7d20f30ce6680f8f6d382ae33b6ed62de14fe78f65edef3f2698d24eb8a' `
+
+    
